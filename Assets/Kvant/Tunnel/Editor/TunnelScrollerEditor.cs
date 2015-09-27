19e87cb159a7c71b9009d1c781aa304fca911e46
Keijiro Takahashi <keijiro@ZX.local>
2015-09-27 11:47:42 +0900

    Added TunnelScrollerEditor.

---
+++ Assets/Kvant/Tunnel/Editor/TunnelScrollerEditor.cs
@@ -0,0 +1,27 @@
+﻿//
+// Custom editor for TunnelScroller
+//
+using UnityEngine;
+using UnityEditor;
+
+namespace Kvant
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TunnelScroller))]
+    public class TunnelScrollerEditor : Editor
+    {
+        SerializedProperty _speed;
+
+        void OnEnable()
+        {
+            _speed = serializedObject.FindProperty("_speed");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_speed);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
